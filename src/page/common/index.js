2a04caaae5a9fa9004c76a1be7c8b80c0366e9d9
showa11 <showa11@hotmail.com>
2017-06-29 23:15:52 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: showa11
+* @Date:   2017-06-29 21:51:01
+* @Last Modified by:   showa11
+* @Last Modified time: 2017-06-29 21:51:52
+*/
+
+'use strict';
+
+console.log('i am global');
---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: showa11
+* @Date:   2017-06-29 19:50:11
+* @Last Modified by:   showa11
+* @Last Modified time: 2017-06-29 22:04:26
+*/
+
+'use strict';
+require('./index.css')
+console.log('hello index');
+
+$('body').html('hello index ~~~');
